bd44cc3939703aec7ebb4299a10b12bf00861241
e3cradha <accenture.radhakrishnan@consulenti.generaligroup.com>
2022-06-15 18:58:39 +0200

    adding validation for request inputs

--- src/main/java/com/knowledgesharing/ms/datatransfer/KnowledgeSharingDto.java
+++ src/main/java/com/knowledgesharing/ms/datatransfer/KnowledgeSharingDto.java
@@ -1,30 +1,32 @@
 package com.knowledgesharing.ms.datatransfer;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class KnowledgeSharingDto {
 
     private Long id;
 
-    @NotNull
+    @NotBlank(message = "the field \"title\" is needed!")
     private String title;
 
-    @NotNull
+    @NotBlank(message = "the field \"author\" is needed!")
     private String author;
 
     private Long likes;
     private Long views;
 
-    @NotNull
+    @NotBlank(message = "the field \"link\" is needed!")
     private String link;
 
     private String date;
